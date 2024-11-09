6ed9ae7ec5add352e461e65af88a3d1030bf9f12
Marcuusvp <marcusviniciuspereira1@gmail.com>
2024-11-09 19:49:28 -0300

    scaffold api

---
+++ Seguros.HttpApi/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Mapster;
+global using MediatR;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
+global using FluentValidation;
